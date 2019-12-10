36ee3b0771de3f165b01ba32cb5d30eb16df1ec6
Javier Arce <jasearce@espol.edu.ec>
2019-12-10 12:06:01 -0500

    commit con app

---
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2t2019_localizacion_de_guardias_para_denuncias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
