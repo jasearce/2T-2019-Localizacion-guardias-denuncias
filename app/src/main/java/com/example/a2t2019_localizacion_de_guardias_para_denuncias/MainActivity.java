597b4985a623bf03a4a035b4240a129befab6991
Javier Arce <jasearce@espol.edu.ec>
2019-12-10 12:58:22 -0500

    inicio de sesion con Google

--- app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/MainActivity.java
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/MainActivity.java
@@ -2,13 +2,107 @@ package com.example.a2t2019_localizacion_de_guardias_para_denuncias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int GOOGLE_SIGN_IN = 123;
+    FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+    }
+
+    /**
+     * Funcion publica para iniciar sesion dentro de la clase pulica MainActivity
+      * @param view
+     */
+    public void iniciarSesion(View view){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
+    }
+
+    /**
+     * Funcion onActivityResult la cual se ejecuta luego de la verificacion de sesion
+     * y obtiene la informacion de sesion o mostrara un error en el Log si el inicio
+     * de sesion ha fallado.
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account != null) firebaseAuthWithGoogle(account);
+            }catch (ApiException e) {
+                Log.w("TAG", "Fallo el inicio de sesión con google.", e);
+            }
+        }
+    }
+
+    /**
+     * Funcion firebaseAuthWithGoogle la cual tiene como objetivo autenticar el inicio de
+     * sesion con Firebase para poder acceder a la base de datos mediantes las credenciales
+     * de Google.
+     * @param acct
+     */
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+            } else {
+                System.out.println("error");
+                updateUI(null);
+            }
+        });
+    }
+
+    /**
+     * Funcion updateUI la cual obtiene los datos del usuario en Firebase
+     * e imprime por consola el nombre del usuario que ingresa
+     * @param user
+     */
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            String photo = String.valueOf(user.getPhotoUrl());
+
+            System.out.println("Nombre");
+            System.out.println(name);
+        } else {
+            System.out.println("Sin registrarse");
+        }
     }
 }
