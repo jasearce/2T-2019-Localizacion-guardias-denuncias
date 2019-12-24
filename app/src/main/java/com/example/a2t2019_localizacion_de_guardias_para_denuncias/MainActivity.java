55ea05806450d853a8a33d3a9154b84bbc547de3
Javier Arce <jasearce@espol.edu.ec>
2019-12-24 17:39:06 -0500

    regreso a version estable

--- app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/MainActivity.java
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/MainActivity.java
@@ -1,118 +1,14 @@
 package com.example.a2t2019_localizacion_de_guardias_para_denuncias;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Varibales públicas
-    static final int GOOGLE_SIGN_IN = 123;
-    FirebaseAuth mAuth;
-    GoogleSignInClient mGoogleSignInClient;
-    // Set the dimensions of the sign-in button.
-    SignInButton signInButton = findViewById(R.id.googleLoginBtn);
-
-
-
-    /*En la función onCreate (Función que se ejecuta al momento de iniciar la acción), instanciamos la configuración de inicio de Google y firebase*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        // Build a GoogleSignInClient with the options specified by gso.
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-    }
-
-    /*Creamos la función pública de iniciar sesión dentro de la clase publica MainActivity.
-     *Dentro de esta función llamaremos al inicio de sesión por Google.
-     */
-    public void iniciarSesion() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
-    }
-
-    /*Metodo de click para iniciar sesion*/
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.googleLoginBtn:
-                iniciarSesion();
-                break;
-             //Agregar el click para el cierre de sesion
-        }
-    }
-
-    /*Implementamos la función (sobrescrita) onActivityResult dentro de la clase publica MainActivity, que se ejecutara después verificar sesión.
-     *Obtendremos la información de sesión o mostraremos un error en el Log.
-     *Si el inicio de sesión ha fallado
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == GOOGLE_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                if (account != null) firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
-                Log.w("TAG", "Fallo el inicio de sesión con google.", e);
-            }
-        }
-    }
-
-    /*Autenticamos sesión con Firebase (para acceder a la base de datos). Para ello usamos las credenciales de de Google.*/
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential).addOnCompleteListener(this, task -> {
-            if (task.isSuccessful()) {
-                FirebaseUser user = mAuth.getCurrentUser();
-                updateUI(user);
-            } else {
-                System.out.println("error");
-                updateUI(null);
-            }
-        });
-    }
-
-    /*Obtenemos el usuario de Firebase. Podemos obtener el nombre, email y foto.
-     *Para verificar que la aplicación función.
-     *Imprime el nombre del usuario
-     */
-    private void updateUI(FirebaseUser user) {
-        if (user != null) {
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            String photo = String.valueOf(user.getPhotoUrl());
-
-            System.out.println("nombre");
-            System.out.println(name);
-
-        } else {
-            System.out.println("sin registrarse");
-        }
     }
 }
