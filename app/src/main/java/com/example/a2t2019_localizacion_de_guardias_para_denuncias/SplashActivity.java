09eb289d258c1f226a5e1e39989cd931de9dee1e
Javier Arce <jasearce@espol.edu.ec>
2019-12-26 11:21:22 -0500

    Se agrega complementos para SplashScreen. Falta el logo de la app al iniciar.

---
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.a2t2019_localizacion_de_guardias_para_denuncias;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
