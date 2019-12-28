fddf5d91b81dfadab8407966ca9a0913e5746903
Javier Arce <jasearce@espol.edu.ec>
2019-12-28 14:10:08 -0500

    Se agrega Firebase al proyecto.

--- app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/LoginActivity.java
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/LoginActivity.java
@@ -3,11 +3,14 @@ package com.example.a2t2019_localizacion_de_guardias_para_denuncias;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    ImageView btnRegresar;
+    private ImageView btnRegresar;
+    private EditText userEditText;
+    private EditText passEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
