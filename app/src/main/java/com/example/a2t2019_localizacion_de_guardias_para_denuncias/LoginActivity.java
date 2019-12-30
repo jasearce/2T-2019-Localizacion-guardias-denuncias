33576f60023421ce6709463fd1cfd4b30d0cf1ef
Javier Arce <jasearce@espol.edu.ec>
2019-12-30 17:32:50 -0500

    Se agrega funcionalidades de Registro e inicio de sesion.

--- app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/LoginActivity.java
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/LoginActivity.java
@@ -1,23 +1,84 @@
 package com.example.a2t2019_localizacion_de_guardias_para_denuncias;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private ImageView btnRegresar;
-    private EditText userEditText;
-    private EditText passEditText;
+    public ImageView btnRegresar;
+    public EditText userEditText;
+    public EditText passEditText;
+
+    public FirebaseAuth mAuth;
+    public FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        userEditText = findViewById(R.id.editTextEmail);
+        passEditText = findViewById(R.id.editTextPass);
+
         btnRegresar = findViewById(R.id.imgRetornar);
         btnRegresar.setOnClickListener(v -> finish());
+
+        mAuth = FirebaseAuth.getInstance();
+        authStateListener = firebaseAuth -> {
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+            if (firebaseUser != null) {
+                Toast.makeText(LoginActivity.this, "Inicio de sesion exitoso",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(LoginActivity.this, "Inicio de sesion falló",Toast.LENGTH_SHORT).show();
+            }
+        };
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener != null){
+            mAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
+    public void login(View view){
+        String email = userEditText.getText().toString();
+        String password = passEditText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(LoginActivity.this,"signInWithEmail:success",
+                            Toast.LENGTH_SHORT).show();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/RegistroActivity.java
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/RegistroActivity.java
@@ -1,14 +1,85 @@
 package com.example.a2t2019_localizacion_de_guardias_para_denuncias;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    public EditText mEmailEt;
+    public EditText mPassEt;
+    public EditText mNombreEt;
+    public EditText mApellidosEt;
+    public EditText mTelefonoEt;
+    public Button btnRegistro;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        mEmailEt = findViewById(R.id.editTextEmail);
+        mPassEt = findViewById(R.id.editTextPass);
+        mNombreEt = findViewById(R.id.editTextNombres);
+        mApellidosEt = findViewById(R.id.editTextApellidos);
+        mTelefonoEt = findViewById(R.id.editTextTelef);
+        btnRegistro = findViewById(R.id.btnRegister);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmailEt.getText().toString().trim();
+                String password = mPassEt.getText().toString().trim();
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    mEmailEt.setError("Email Invalido!");
+                    mEmailEt.setFocusable(true);
+                }else{
+                    registrar(email,password);
+                }
+            }
+        });
+
+    }
+
+    private void registrar(String email,String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(RegistroActivity.this, "Registro exitoso",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegistroActivity.this, "Autenticacion falló",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegistroActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
