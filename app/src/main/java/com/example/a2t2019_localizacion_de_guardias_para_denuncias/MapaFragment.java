ba3f699c77ab9515e5007ee4369d6f12e70544e0
Javier Arce <jasearce@espol.edu.ec>
2020-01-07 22:07:28 -0500

    Se agrega Mapa al MapaFragment pero con un error. Corregirlo

--- app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/MapaFragment.java
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/MapaFragment.java
@@ -3,29 +3,70 @@ package com.example.a2t2019_localizacion_de_guardias_para_denuncias;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapaFragment extends Fragment {
-
+public class MapaFragment extends Fragment implements OnMapReadyCallback {
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
 
     public MapaFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa, container, false);
+        mView =  inflater.inflate(R.layout.fragment_mapa, container, false);
+        return mView;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mMapView = (MapView) mView.findViewById(R.id.mapaGuardias);
+        if(mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247, -74.044502))
+                .title("Estatua de la Libertad"));
+        CameraPosition liberty = CameraPosition.builder().target(new LatLng(40.689247, -74.044502))
+                .zoom(16)
+                .bearing(0)
+                .tilt(45)
+                .build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(liberty));
+    }
 }
