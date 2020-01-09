52abfd9bf69a2ddefc466c8adba1f6ab75279b19
Javier Arce <jasearce@espol.edu.ec>
2020-01-09 12:09:05 -0500

    Cambio en tipo de botones para denuncia. Colocar imagenes de fondo.

--- app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/HomeFragment.java
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/HomeFragment.java
@@ -8,12 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    Button btnRobo, btnAgresion, btnDrogas, btnOtroDelito;
 
 
     public HomeFragment() {
@@ -25,7 +28,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        btnRobo = view.findViewById(R.id.btnDenunciaRobo);
+        btnAgresion = view.findViewById(R.id.btnDenunciaAgresion);
+        btnDrogas = view.findViewById(R.id.btnDenunciaDrogas);
+        btnOtroDelito = view.findViewById(R.id.btnDenunciaOtro);
+
+        return view;
     }
 
 }
