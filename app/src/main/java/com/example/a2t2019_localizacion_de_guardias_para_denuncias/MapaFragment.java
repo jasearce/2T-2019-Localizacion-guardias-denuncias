dcbb412663ee1b88103bc750486b1a5c03cdf7e3
Joselo Soto <jdsoto@espol.edu.ec>
2020-01-10 02:27:40 -0500

    Se agregaron marcadores al mapa para los guardias

--- app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/MapaFragment.java
+++ app/src/main/java/com/example/a2t2019_localizacion_de_guardias_para_denuncias/MapaFragment.java
@@ -20,6 +20,7 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -80,6 +81,7 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback {
 
         UiSettings uiSettings = mGoogleMap.getUiSettings();
         uiSettings.setZoomControlsEnabled(true);
+        mGoogleMap.setMyLocationEnabled(true);
 
         mDatabase.child("Ubicacion").addValueEventListener(new ValueEventListener() {
             @Override
@@ -89,17 +91,18 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback {
                 for(Marker marker:realTimeMarkers){
                     marker.remove();
                 }
-                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()) {
                     Ubicacion ubi = snapshot.getValue(Ubicacion.class);
                     Double latitud = ubi.getLatitud();
                     Double longitud = ubi.getLongitud();
 
                     MarkerOptions markerOptions = new MarkerOptions();
+                    agregarGuardias();
                     markerOptions.position(new LatLng(latitud, longitud));
                     tmpRealTimeMarkers.add(mGoogleMap.addMarker(markerOptions));
 
                     CameraPosition cameraPosition = CameraPosition.builder().target(new LatLng(latitud, longitud))
-                            .zoom(10)
+                            .zoom(15)
                             .bearing(0)
                             .tilt(45)
                             .build();
@@ -123,5 +126,28 @@ public class MapaFragment extends Fragment implements OnMapReadyCallback {
                 .tilt(45)
                 .build();
         */
+    }
+
+    private void agregarGuardias() {
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.1438333392739195, -79.96212478609135)).title("EDCOM").snippet("Guardia en EDCOM").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.1458741000000003,-79.94890597407598)).title("PARCOM").snippet("Guardia en PARCOM").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.1502017,-79.9495475)).title("ADMISIONES").snippet("Guardia en ADMISIONES").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.1520112253733545, -79.9533235538422)).title("Garita").snippet("Guardias en Garita").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.1519958666914074,-79.95336846312456)).title("Biciletas Terminal").snippet("Guardias en Terminal de Biciletas").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.146772,-79.966155)).title("ASIRI").snippet("Guardia Parqueaderos Biblioteca central por ASIRI").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.148893,-79.967628)).title("CELEX").snippet("Guardia Parqueaderos CELEX parte de atras").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.1514567091350516,-79.95454509409174)).title("CIBE").snippet("Guardia en CIBE").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.1521011479322807,-79.95594879937659)).title("FCV").snippet("Guardia en FCV").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.148348477036179, -79.96371562917663)).title("RECTORADO").snippet("Guardia en RECTORADO").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.1468200881734965,-79.96302771313425)).title("MARITIMA").snippet("Guardia en MARITIMA").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.15237555, -79.95822722537349)).title("PISCINA").snippet("Guardia en PISCINA").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-2.145925806984807, -79.9647382958111)).title("FICT").snippet("Guardia en parqueadero de FICT, frente al Coliseo").icon(BitmapDescriptorFactory.fromResource(R.drawable.police2)));
+
+
+
+
+
+
+
     }
 }
